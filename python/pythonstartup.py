d63b9a1e654c4e6205956cf22cf9926b6437c32c
osmannyildiz <iamosmannyildiz@gmail.com>
2024-07-07 22:25:12 +0300

    refactor(python/pythonstartup): adjust empty lines

--- python/pythonstartup.py
+++ python/pythonstartup.py
@@ -2,8 +2,5 @@
 # https://stackoverflow.com/questions/9730409/exiting-from-python-command-line
 type(exit).__repr__ = type(exit).__call__
 
-
 # Import pprint by default
 from pprint import pprint
-
-
