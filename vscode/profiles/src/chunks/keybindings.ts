9bbde6dcaa9848d3f900b2ff726d02f3e173c272
osmannyildiz <iamosmannyildiz@gmail.com>
2024-07-16 15:04:04 +0300

    feat(vscode/profiles): update keybindings

--- vscode/profiles/src/chunks/keybindings.ts
+++ vscode/profiles/src/chunks/keybindings.ts
@@ -1,6 +1,171 @@
 import type { KeybindingsChunk } from "../definitions";
 
 export const mac: KeybindingsChunk = [
+	// ===== MULTI CURSOR =====
+	// alt+cmd+up => editor.action.insertCursorAbove
+	// alt+cmd+down => editor.action.insertCursorBelow
+
+	// ===== DUPLICATE LINE(S) =====
+	// shift+alt+up => editor.action.copyLinesUpAction
+	// shift+alt+down => editor.action.copyLinesDownAction
+
+	// ===== DELETE LINE(S) =====
+	// shift+backspace => editor.action.deleteLines
+	// cmd+backspace => deleteAllLeft
+	// cmd+delete => deleteAllRight
+	{
+		key: "shift+backspace",
+		command: "editor.action.deleteLines",
+		when: "textInputFocus && !editorReadonly",
+	},
+	{
+		key: "shift+backspace",
+		command: "-deleteLeft",
+		when: "textInputFocus",
+	},
+	{
+		key: "shift+cmd+k",
+		command: "-editor.action.deleteLines",
+		when: "textInputFocus && !editorReadonly",
+	},
+
+	// ===== CAMELHUMPS =====
+	{
+		key: "alt+right",
+		command: "cursorWordPartRight",
+		when: "textInputFocus",
+	},
+	{
+		key: "ctrl+alt+right",
+		command: "-cursorWordPartRight",
+		when: "textInputFocus",
+	},
+	{
+		key: "alt+right",
+		command: "-cursorWordEndRight",
+		when: "textInputFocus",
+	},
+	{
+		key: "shift+alt+right",
+		command: "cursorWordPartRightSelect",
+		when: "textInputFocus",
+	},
+	{
+		key: "ctrl+shift+alt+right",
+		command: "-cursorWordPartRightSelect",
+		when: "textInputFocus",
+	},
+	{
+		key: "shift+alt+right",
+		command: "-cursorWordEndRightSelect",
+		when: "textInputFocus",
+	},
+	{
+		key: "alt+left",
+		command: "cursorWordPartLeft",
+		when: "textInputFocus",
+	},
+	{
+		key: "ctrl+alt+left",
+		command: "-cursorWordPartLeft",
+		when: "textInputFocus",
+	},
+	{
+		key: "alt+left",
+		command: "-cursorWordLeft",
+		when: "textInputFocus",
+	},
+	{
+		key: "shift+alt+left",
+		command: "cursorWordPartLeftSelect",
+		when: "textInputFocus",
+	},
+	{
+		key: "ctrl+shift+alt+left",
+		command: "-cursorWordPartLeftSelect",
+		when: "textInputFocus",
+	},
+	{
+		key: "shift+alt+left",
+		command: "-cursorWordLeftSelect",
+		when: "textInputFocus",
+	},
+	{
+		key: "alt+backspace",
+		command: "deleteWordPartLeft",
+		when: "textInputFocus && !editorReadonly",
+	},
+	{
+		key: "ctrl+alt+backspace",
+		command: "-deleteWordPartLeft",
+		when: "textInputFocus && !editorReadonly",
+	},
+	{
+		key: "alt+backspace",
+		command: "-deleteWordLeft",
+		when: "textInputFocus && !editorReadonly",
+	},
+	{
+		key: "alt+delete",
+		command: "deleteWordPartRight",
+		when: "textInputFocus && !editorReadonly",
+	},
+	{
+		key: "ctrl+alt+delete",
+		command: "-deleteWordPartRight",
+		when: "textInputFocus && !editorReadonly",
+	},
+	{
+		key: "alt+delete",
+		command: "-deleteWordRight",
+		when: "textInputFocus && !editorReadonly",
+	},
+
+	// ===== TOGGLE LINE COMMENT =====
+	// cmd+shift+7 => editor.action.commentLine
+
+	// ===== ADD PREVIOUS OCCURRENCE TO SELECTION =====
+	// cmd+d => addSelectionToNextFindMatch
+	// shift+cmd+d => addSelectionToPreviousFindMatch
+	{
+		key: "shift+cmd+d",
+		command: "editor.action.addSelectionToPreviousFindMatch",
+		when: "editorFocus",
+	},
+	{
+		key: "shift+cmd+d",
+		command: "-workbench.view.debug",
+		when: "viewContainer.workbench.view.debug.enabled",
+	},
+
+	// ===== NEW LINE BELOW =====
+	// cmd+enter => editor.action.insertLineAfter
+
+	// ===== SAVE ALL =====
+	// ctrl+alt+cmd+s => workbench.action.files.saveFiles
+	{
+		key: "ctrl+alt+cmd+s",
+		command: "workbench.action.files.saveFiles",
+	},
+
+	// ===== SAVE WITHOUT FORMATTING =====
+	// ctrl+cmd+s => workbench.action.files.saveWithoutFormatting
+	{
+		key: "ctrl+cmd+s",
+		command: "workbench.action.files.saveWithoutFormatting",
+	},
+	{
+		key: "cmd+k s",
+		command: "-workbench.action.files.saveWithoutFormatting",
+	},
+
+	// ===== TOGGLE TERMINAL =====
+	{
+		key: "ctrl+[IntlBackslash]",
+		command: "workbench.action.terminal.toggleTerminal",
+	},
+
+	// ===== DON'T INTERCEPT CTRL+Q IN TERMINAL =====
 	{
 		key: "ctrl+q",
 		command: "-workbench.action.quickOpenView",
@@ -10,6 +175,9 @@ export const mac: KeybindingsChunk = [
 		command: "workbench.action.quickOpenView",
 		when: "!terminalFocus",
 	},
+
+	// ===== MISC. =====
+	// ctrl+cmd+p => workbench.action.quickTextSearch
 	{
 		key: "ctrl+cmd+p",
 		command: "workbench.action.quickTextSearch",
@@ -18,6 +186,8 @@ export const mac: KeybindingsChunk = [
 
 export const regular: KeybindingsChunk = [
 	// ===== MULTI CURSOR =====
+	// ctrl+alt+up => editor.action.insertCursorAbove
+	// ctrl+alt+down => editor.action.insertCursorBelow
 	{
 		key: "ctrl+alt+up",
 		command: "editor.action.insertCursorAbove",
@@ -50,6 +220,8 @@ export const regular: KeybindingsChunk = [
 	},
 
 	// ===== DUPLICATE LINE(S) =====
+	// shift+alt+up => editor.action.copyLinesUpAction
+	// shift+alt+down => editor.action.copyLinesDownAction
 	{
 		key: "shift+alt+up",
 		command: "editor.action.copyLinesUpAction",
@@ -72,16 +244,17 @@ export const regular: KeybindingsChunk = [
 	},
 
 	// ===== DELETE LINE(S) =====
+	// shift+delete => editor.action.deleteLines
 	{
 		key: "shift+delete",
 		command: "editor.action.deleteLines",
 		when: "textInputFocus && !editorReadonly",
 	},
-	// {
-	// 	key: "ctrl+shift+k",
-	// 	command: "-editor.action.deleteLines",
-	// 	when: "textInputFocus && !editorReadonly",
-	// },
+	{
+		key: "ctrl+shift+k",
+		command: "-editor.action.deleteLines",
+		when: "textInputFocus && !editorReadonly",
+	},
 
 	// ===== CAMELHUMPS =====
 	{
@@ -170,28 +343,28 @@ export const regular: KeybindingsChunk = [
 	},
 
 	// ===== CHANGE ALL OCCURRENCES =====
-	{
-		key: "f2",
-		command: "editor.action.changeAll",
-		when: "editorTextFocus && !editorReadonly",
-	},
-	{
-		key: "ctrl+f2",
-		command: "-editor.action.changeAll",
-		when: "editorTextFocus && !editorReadonly",
-	},
+	// {
+	// 	key: "f2",
+	// 	command: "editor.action.changeAll",
+	// 	when: "editorTextFocus && !editorReadonly",
+	// },
+	// {
+	// 	key: "ctrl+f2",
+	// 	command: "-editor.action.changeAll",
+	// 	when: "editorTextFocus && !editorReadonly",
+	// },
 
 	// ===== RENAME SYMBOL =====
-	{
-		key: "ctrl+f2",
-		command: "editor.action.rename",
-		when: "editorHasRenameProvider && editorTextFocus && !editorReadonly",
-	},
-	{
-		key: "f2",
-		command: "-editor.action.rename",
-		when: "editorHasRenameProvider && editorTextFocus && !editorReadonly",
-	},
+	// {
+	// 	key: "ctrl+f2",
+	// 	command: "editor.action.rename",
+	// 	when: "editorHasRenameProvider && editorTextFocus && !editorReadonly",
+	// },
+	// {
+	// 	key: "f2",
+	// 	command: "-editor.action.rename",
+	// 	when: "editorHasRenameProvider && editorTextFocus && !editorReadonly",
+	// },
 
 	// ===== SAVE ALL =====
 	{
